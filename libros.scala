675e76d1daeb35e36237fbd18404347d7200f46b
amenpunk <ondasycircuitos@gmail.com>
2023-07-02 22:12:41 -0600

    menu

--- libros.scala
+++ libros.scala
@@ -1,8 +1,9 @@
 import scala.collection.mutable.ListBuffer
+import scala.io.StdIn
 
 case class Libros (id:Int,titulo:String,autor:String,publicacion:Int)
 
-object Main extends App {
+object Libros {
 
   val libros : ListBuffer[Libros] = ListBuffer()
 
@@ -40,17 +41,51 @@ object Main extends App {
     }
   }
 
-  // TODO: Implementar las funciones actualizarLibro y eliminarLibro
-
+  // // TODO: Implementar las funciones actualizarLibro y eliminarLibro
+  //
   agregarLibro(1,"Libro 1","Autor 1",1943)
   agregarLibro(2,"Libro 2","Autor 2",1999)
   agregarLibro(3,"Libro 3","Autor 4",2000)
   agregarLibro(4,"El principito","Antoine de Saint-Exupéry",1943)
+  //
+  // actualizarLibro(4,"La principita","Antoine de Saint-Exupéry",1552)
+  // eliminarLibro(2)
+  //
+  // listarLibros()
 
-  actualizarLibro(4,"La principita","Antoine de Saint-Exupéry",1552)
-  eliminarLibro(2)
+ //
+  def mostrarMenu(): Unit = {
+    println("1. Agregar libro")
+    println("2. Buscar libro")
+    println("3. Actualizar libro")
+    println("4. Eliminar libro")
+    println("5. Listar libros")
+    println("6. Salir")
+  }
+ //
+ //
+ //
+  def main(args: Array[String]): Unit = {
+    var opcion = 0
+    while (opcion != 4) {
+      mostrarMenu()
+      opcion = StdIn.readInt()
+      // seleccionarOpcion(opcion)
+    }
+    // listarLibros()
+  }
+ //
+ //  def seleccionarOpcion(opcion: Int): Unit = {
+ //    opcion match {
+ //      case 1 => println("Has seleccionado la Opción 1")
+ //      case 2 => println("Has seleccionado la Opción 2")
+ //      case 3 => println("Has seleccionado la Opción 3")
+ //      case 4 => println("Saliendo del programa...")
+ //      case _ => println("Opción inválida")
+ //    }
+ //    // println()
+ //  }
 
-  listarLibros()
 
 
 }
