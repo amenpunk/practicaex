e12e3ba003e157db2326cf2e372abd94397cc7e2
amenpunk <ondasycircuitos@gmail.com>
2023-07-02 20:29:24 -0600

    ejercicio completo scala

--- libros.scala
+++ libros.scala
@@ -1,6 +1,6 @@
 import scala.collection.mutable.ListBuffer
 
-case class Libros (titulo:String,autor:String,publicacion:Int)
+case class Libros (id:Int,titulo:String,autor:String,publicacion:Int)
 
 object Main extends App {
 
@@ -10,8 +10,8 @@ object Main extends App {
     libros.foreach(println)
   }
 
-  def agregarLibro(titulo:String,autor:String,publicacion:Int): Unit = {
-    val libro = Libros(titulo,autor,publicacion)
+  def agregarLibro(id:Int,titulo:String,autor:String,publicacion:Int): Unit = {
+    val libro = Libros(id,titulo,autor,publicacion)
     libros += libro
   }
 
@@ -19,7 +19,37 @@ object Main extends App {
     libros.find(_.id == id)
   } 
 
-  agregarLibro("El principito","Antoine de Saint-Exupéry",1943)
+  def eliminarLibro(id: Int) : Boolean = {
+    buscarLibro(id) match {
+      case Some(libro) => {
+        libros -= libro
+        true
+      }
+      case None => false
+    }
+  }
+
+  def actualizarLibro(id:Int, titulo:String, autor:String, publicacion:Int):Boolean = {
+    buscarLibro(id) match {
+      case Some(libro) => {
+        val indice = libros.indexOf(libro)
+        libros(indice) = libro.copy(titulo = titulo, autor = autor, publicacion = publicacion)
+        true
+      }
+      case None => false
+    }
+  }
+
+  // TODO: Implementar las funciones actualizarLibro y eliminarLibro
+
+  agregarLibro(1,"Libro 1","Autor 1",1943)
+  agregarLibro(2,"Libro 2","Autor 2",1999)
+  agregarLibro(3,"Libro 3","Autor 4",2000)
+  agregarLibro(4,"El principito","Antoine de Saint-Exupéry",1943)
+
+  actualizarLibro(4,"La principita","Antoine de Saint-Exupéry",1552)
+  eliminarLibro(2)
+
   listarLibros()
 
 
